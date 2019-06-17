4b9e25c4bc6268ce8d13216a95b398ef3da10f55
azimshaik <cheer.buddu@gmail.com>
2019-06-17 19:32:05 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
+import { API } from "aws-amplify";
+import { withAuthenticator } from "aws-amplify-react";
 function App() {
+  const [people, updatePeople] = useState([]);
+  const [searchString, updateSearchString] = useState("");
+  const [searchData, updateSearchData] = useState([]);
+  async function callApi() {
+    try {
+      const peopleData = await API.get("mainappapi", "/people");
+      console.log("peopleData:", peopleData);
+      updatePeople(peopleData.people);
+
+      // const coinData = await API.get("mainappapi", "/coins");
+      // console.log("coinData:", coinData);
+    } catch (err) {
+      console.log({ err });
+    }
+  }
+  // useEffect(() => {
+  //   callApi();
+  // }, []);
+  async function search() {
+    try {
+      const searchData = await API.post("mainappapi", "/jobs", {
+        body: { search: searchString }
+      });
+      console.log("searchData:", searchData);
+    } catch (err) {
+      console.log({ err });
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world</h1>
+      <input onChange={e => updateSearchString(e.target.value)} />
+      <button onClick={search}>Search</button>
+      {searchData.map((j, i) => (
+        <div>
+          <h2>{j.title}</h2>
+          <p>{j.description}</p>
+        </div>
+      ))}
     </div>
   );
 }
 
 export default App;
+// export default withAuthenticator(App);
